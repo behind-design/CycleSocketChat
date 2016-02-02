dd6ace02245673034f28da59ced30ebf34531c00
kevinle <pragmaticobjects@gmail.com>
2016-02-02 02:28:25 +0000

    remove placeholder code

--- client/src/chatPane.js
+++ client/src/chatPane.js
@@ -6,22 +6,6 @@ export function chatPane(DOMSource) {
   let vdom$ = Observable.of(textValue =>
     div([      
     ])
-                             
-    /*                             
-    div([
-      h4('Chat Messages'),
-      div({className: 'row'}, [
-        div({className: 'input-field col s10'}, [
-          input({id: 'input-msg', className: 'validate', value: textValue}),
-          label({className: 'active'}, 'Type your chat, enter or hit button to send')
-        ]),
-        div({className: 'input-field col s2'}, [
-          a({id: 'send-btn', className: 'btn-floating btn-large waves-effect waves-light red'}, [
-            i({className: 'material-icons'}, 'send')
-          ]) 
-        ]),
-      ])
-    ])*/
   );    
   
   return vdom$;
